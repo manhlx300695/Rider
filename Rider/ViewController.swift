009fc1d347ee4664d748a223d889d2ffefbe8579
Mike <mike.le@grabtaxi.com>
2017-01-26 14:27:27 +0800

    (FEAT) Create Rider Xcode proj

---
+++ Rider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rider
+//
+//  Created by Xuan Manh Le on 26/1/17.
+//  Copyright © 2017 Xuan Manh Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
